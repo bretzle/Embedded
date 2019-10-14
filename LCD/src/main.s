25d65c9c93e935ce495a6760ce6f5c4e98fe0d9a
John Bretz <bretzj@msoe.edu>
2019-10-14 15:41:54 -0500

    finished main subroutine

--- LCD/src/main.s
+++ LCD/src/main.s
@@ -9,34 +9,33 @@
 .thumb
 .section .text
 
-#Fill in addresses
-	.equ RCC_BASE, 0x40023800
-	.equ RCC_AHB1ENR, 0x30
-	.equ RCC_GPIOAEN, 0x1
-	.equ RCC_GPIOCEN, 0x4
-
-	.equ GPIOA_BASE, 0x40020000
-	.equ GPIOC_BASE, 0x40020800
-	.equ GPIO_MODER, 0x0
-	.equ GPIO_ODR, 0x14
-	.equ BSRR_OFFSET, 0x18
-
-#What pin is each of these?
-	.equ RS, 0x8
-	.equ RW, 0x9
-	.equ E,  0xA
-
-#Commands for BSRR
-	.equ E_SET,  0x400
-	.equ RW_SET, 0x200
-	.equ RS_SET, 0x100
-
 .global main
 
 main:
 	bl LcdInit
 
-	mov R1, #8
+start:
+	@ print valid number
+	mov R1, #1234
 	bl lcd_print_num
-end:
-	b end
+
+	@ wait a second
+	mov R2, #1000
+	bl delay_ms
+
+	bl lcd_clear
+
+	@ print an invalid number
+	mov R1, #0xFFFF
+	bl lcd_print_num
+
+	mov R2, #1000
+	bl delay_ms
+
+	mov R1, string
+	bl lcd_print_string
+
+	b start
+
+.section .data
+	string: .asciz "Im a string"
