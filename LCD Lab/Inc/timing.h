27e8627cc19f1d8528a5c72b6da29ce8af593f57
John Bretz <bretzj@msoe.edu>
2020-01-07 10:58:49 -0600

    Added docs to timing.h

--- LCD Lab/Inc/timing.h
+++ LCD Lab/Inc/timing.h
@@ -1,10 +1,17 @@
-/* header file */
 #include <inttypes.h>
 
-/* all symbolic numbers */
+#ifndef TIMING_H
+#define TIMING_H
+
 #define F_CPU 16000000UL
 #define SYSTICK (volatile uint32_t *) 0xE000E010
 
-/* public method prototypes */
+/// A blocking delay that will last N milliseconds
+/// @param num the number of milliseconds to delay for
 void delay_ms(uint32_t);
+
+/// A blocking delay that will last N microseconds
+/// @param num the number of microseconds to delay for
 void delay_us(uint32_t);
+
+#endif
