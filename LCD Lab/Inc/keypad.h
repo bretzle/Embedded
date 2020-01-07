a5684b1e45d81a4790a3f63ff3b38b043968ec3f
John Bretz <bretzj@msoe.edu>
2020-01-07 10:55:07 -0600

    Added docs to lcd.h + keypad.h

--- LCD Lab/Inc/keypad.h
+++ LCD Lab/Inc/keypad.h
@@ -2,12 +2,22 @@
 #ifndef KEYPAD_H
 #define KEYPAD_H
 
+/// Initializes the keypad
 void keypad_init(void);
 
+/// Returns a keycode of a key press.
+/// Function will not return untill a key is pressed.
+/// @return returns 0 - 15
 int keypad_getkey(void);
 
+/// Returns a keycode of a key press.
+/// Similar to keypad_getkey() but does not block.
+/// @return returns 0 - 15 but if no key was pressed -1
 int keypad_getkey_noblock(void);
 
+/// Returns an ascii char of the key press
+/// Function will not return untill a key is pressed.
+/// @return the ascii char
 char keypad_getchar(void);
 
 #endif
--- LCD Lab/Inc/lcd.h
+++ LCD Lab/Inc/lcd.h
@@ -1,14 +1,30 @@
+#ifndef LCD_H
+#define LCD_H
 
+/// Initializes the LCD
 void lcd_init(void);
 
+/// Clears the LCD. Includes delay
 void lcd_clear(void);
 
+/// Moves the cursor back home. Includes delay
 void lcd_home(void);
 
+/// Moves the cursor to some location. Includes delay
+/// @param row top row is 0, bottom row is 1
+/// @param col column is 0 to 39. Only 0 to 15 are visible
 void lcd_set_position(char row, char col);
 
+/// Prints a string to the LCD. Does not word wrap. Includes delay.
+/// @param string the string to print
 void lcd_print_string(char string[]);
 
+/// Prints an integer to the LCD. Does not wrap. Includes delay.
+/// @param number the int to print
 void lcd_print_num(int number);
 
+/// Print a single ascii charcter to the LCD. Includes delay.
+/// @param charcter an ascii char
 void lcd_print_char(char character);
+
+#endif
