8f2f85c120f4c03321c5ac58b5ec079bdea873ec
John Bretz <bretzj@msoe.edu>
2020-01-21 17:04:08 -0600

    Made put & get blocking

--- Embedded II/Ring Buffer/Src/ring_buffer.c
+++ Embedded II/Ring Buffer/Src/ring_buffer.c
@@ -3,31 +3,38 @@
 #define ARRAY_LENGTH(array) sizeof(array)/sizeof(array[0])
 
 void put(RingBuffer * ring, char byte) {
-    (*ring).buffer[(*ring).put] = byte;
+    while (has_space(ring)) {
+        (*ring).buffer[(*ring).put] = byte;
 
-    if ((*ring).put == ARRAY_LENGTH((*ring).buffer) - 1) {
-        (*ring).put = 0;
-    } else {
-        (*ring).put += 1;
+        if ((*ring).put == ARRAY_LENGTH((*ring).buffer) - 1) {
+            (*ring).put = 0;
+        } else {
+            (*ring).put += 1;
+        }
     }
 }
 
 char get(RingBuffer * ring) {
-    int value = (*ring).buffer[(*ring).get];
+    char value;
 
-    if ((*ring).get == ARRAY_LENGTH((*ring).buffer) - 1) {
-        (*ring).get = 0;
-    } else {
-        (*ring).get += 1;
+    while (has_space(ring)) {
+        value = (*ring).buffer[(*ring).get];
+        if ((*ring).get == ARRAY_LENGTH((*ring).buffer) - 1) {
+            (*ring).get = 0;
+        } else {
+            (*ring).get += 1;
+        }
     }
 
     return value;
 }
 
+// todo
 int has_space(RingBuffer * ring) {
-	return 0;
+	return 1;
 }
 
+// todo
 int has_element(RingBuffer * ring) {
-	return 0;
+	return 1;
 }
