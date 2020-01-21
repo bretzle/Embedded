85e7109d4bbfa06cf313e061c4a6df40693ebe36
John Bretz <bretzj@msoe.edu>
2020-01-21 17:00:10 -0600

    cleaned up ring buffer a bit

--- Embedded II/Ring Buffer/Inc/ring_buffer.h
+++ Embedded II/Ring Buffer/Inc/ring_buffer.h
@@ -9,9 +9,15 @@ typedef struct {
     char buffer[BUFFER_SIZE];
 } RingBuffer;
 
+// Puts a new character into the buffer
+// Will block until their is space to fit a new element
 void put(RingBuffer *, char);
+// Gets the next character from the buffer
+// Will block if the buffer is empty
 char get(RingBuffer *);
+// returns true if their is room for at least one element in the buffer
 int has_space(RingBuffer *);
+// returns true if their is at least one room in the buffer
 int has_element(RingBuffer *);
 
 #endif
