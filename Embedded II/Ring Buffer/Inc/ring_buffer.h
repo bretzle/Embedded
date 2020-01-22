81553e02769111d730094342ac28a47587db02df
John Bretz <bretzj@msoe.edu>
2020-01-22 11:01:05 -0600

    Added a non blocking version of put and get

--- Embedded II/Ring Buffer/Inc/ring_buffer.h
+++ Embedded II/Ring Buffer/Inc/ring_buffer.h
@@ -12,13 +12,21 @@ typedef struct {
 
 // Puts a new character into the buffer
 // Will block until their is space to fit a new element
-void put(RingBuffer *, char);
+void put_b(RingBuffer *, char);
 // Gets the next character from the buffer
 // Will block if the buffer is empty
-char get(RingBuffer *);
+char get_b(RingBuffer *);
 // returns true if their is room for at least one element in the buffer
 int has_space(RingBuffer *);
 // returns true if their is at least one room in the buffer
 int has_element(RingBuffer *);
+
+// Will try to add an element into the buffer
+// Will return non-zero if element added else zero
+char put(RingBuffer *, char);
+
+// Will try to get an element from the buffer
+// Will return the element if retrieved else -1
+char get(RingBuffer *);
 
 #endif
--- Embedded II/Ring Buffer/Src/ring_buffer.c
+++ Embedded II/Ring Buffer/Src/ring_buffer.c
@@ -2,39 +2,56 @@
 
 #define ARRAY_LENGTH(array) sizeof(array)/sizeof(array[0])
 
-void put(RingBuffer * ring, char byte) {
+void put_b(RingBuffer * ring, char byte) {
     while (1) {
-        if (has_space(ring)) {
-            (*ring).buffer[(*ring).put] = byte;
-            (*ring).amount += 1;
+        if (put(ring, byte)) {
+            return;
+        }
+    }
+}
 
-            if ((*ring).put == ARRAY_LENGTH((*ring).buffer) - 1) {
-                (*ring).put = 0;
-            } else {
-                (*ring).put += 1;
-            }   
-            return;    
+char put(RingBuffer * ring, char byte) {
+    if (has_space(ring)) {
+        (*ring).buffer[(*ring).put] = byte;
+        (*ring).amount += 1;
+
+        if ((*ring).put == ARRAY_LENGTH((*ring).buffer) - 1) {
+            (*ring).put = 0;
+        } else {
+            (*ring).put += 1;
+        }   
+
+        return 1;
+    }
+    return 0;
+}
+
+char get_b(RingBuffer * ring) {
+    char value;
+
+    while (1) {
+        value = get(ring);
+        if (value != -1) {
+            return value;
         }
     }
 }
 
 char get(RingBuffer * ring) {
-    char value;
+    if (has_element(ring)) {
+        char value = (*ring).buffer[(*ring).get];
+        (*ring).amount -= 1;
 
-    while (1) {
-        if (has_element(ring)) {
-            value = (*ring).buffer[(*ring).get];
-            (*ring).amount -= 1;
-
-            if ((*ring).get == ARRAY_LENGTH((*ring).buffer) - 1) {
-                (*ring).get = 0;
-            } else {
-                (*ring).get += 1;
-            }
-
-            return value;
+        if ((*ring).get == ARRAY_LENGTH((*ring).buffer) - 1) {
+            (*ring).get = 0;
+        } else {
+            (*ring).get += 1;
         }
+
+        return value;
     }
+
+    return -1;
 }
 
 int has_space(RingBuffer * ring) {
